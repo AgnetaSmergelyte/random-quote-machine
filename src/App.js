f57d58200aac7359918b1d4786d653e652bb1dd2
spegisima <agneta.smergelyte@gmail.com>
2023-08-29 11:21:47 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+
 
 function App() {
+
+  const [color, setColor] = useState('');
+  const [quote, setQuote] = useState({text: '', author: ''})
+
+
+  useEffect(() => {
+      newQuote();
+  }, [])
+
+   function newQuote() {
+       const randomColor = Math.floor(Math.random()*16777215).toString(16);
+       setColor('#' + randomColor);
+
+       const options = {
+           method: 'GET',
+           headers: { 'X-Api-Key': 'Hzw9wiAG7dv910xEJQs4jQ==uEjuHLpd5AbPXyC4'},
+           contentType: 'application/json'
+       }
+       fetch ('https://api.api-ninjas.com/v1/quotes?category=dreams', options)
+           .then (res => res.json())
+           .then (data => {
+               setQuote({text: data[0].quote, author: data[0].author});
+           })
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" style={{backgroundColor: color, color: color}}>
+        <div id="quote-box">
+            <h1 id="text">{quote.text}</h1>
+            <div id="author">- {quote.author}</div>
+            <div className="buttons">
+                <a style={{backgroundColor: color}} id="tweet-quote" href={`https://twitter.com/intent/tweet?text="${quote.text}" ${quote.author}`} target="_blank">
+                    <img src="https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/twitter-icon-18-256.png" alt=""/>
+                </a>
+                <button style={{backgroundColor: color}} id="new-quote" onClick={newQuote}>New quote</button>
+            </div>
+        </div>
     </div>
   );
 }
